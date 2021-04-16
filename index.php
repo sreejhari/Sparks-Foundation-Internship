71b2ef5b20a2a6e351cb83ff671d606ffb429a4d
sreejhari <82589272+sreejhari@users.noreply.github.com>
2021-04-16 11:33:30 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Banking System</title>
+    <link rel=" stylesheet" href="css/style.css">
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+</head>
+<style>
+body {
+  background-image: url('back/back.jpg');
+}
+</style>
+<body>
+<div class="container intro">
+<center><lottie-player src="https://assets2.lottiefiles.com/private_files/lf30_jcn1rjuu.json"  background="transparent"  speed="1"  style="width: 500px; height: 500px;"  loop  autoplay></lottie-player></center>
+    <h2><u>THE SPARKS FOUNDATION<u></h2>
+   <h1><u>Web Development Intern<u></h1>
+    
+    <h1><u>TASK 1(Technical)<u></h1>
+<h1><u>BASIC BANKING SYSTEM<u></h1>
+    
+    <center><h2 class="name"><u> DESIGNED BY:-SANDLI SAMAIYA<u></h2></center>
+<a href="./index1.php" class="btn">Get Started</a>
+    
+    
+    
+</div>
+</html>
+</body>
