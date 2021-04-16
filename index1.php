2ea8016cbaad84d52a56f67024efa78961d01a43
sreejhari <82589272+sreejhari@users.noreply.github.com>
2021-04-16 11:34:22 +0530

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Banking System</title>
+  <link rel="stylesheet" href="css/style.css">
+   <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+</head>
+<style>
+body {
+  background-image: url('back/image.jpg');
+}
+</style>
+<body>
+ 
+  <div >
+    <center><lottie-player src="https://assets2.lottiefiles.com/packages/lf20_smp3TG.json"  background="transparent"  speed="1"  style="width: 500px; height: 500px;"  loop  autoplay></lottie-player></center>
+    <section class="container heading-wrapper">
+      <h1><u>Welcome to<u></h1>
+      <h1><u>HLC BANK<u></h1>
+      <br>
+      <br>
+      <br>
+      <p>Instant Transfer !</p>
+      <br>
+      <p>Transfer Your Money With Ease.</p>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <a href="./all_users.php">View Customers</a> 
+    </section>
+  </div>
+  
+</body>
+</html>
